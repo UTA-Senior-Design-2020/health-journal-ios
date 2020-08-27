cd158486a1e4611cb3087761871899b85f84a669
Sadrac Tijerina <reavity3@gmail.com>
2020-08-27 14:44:25 -0500

    Got a task to show up, and structured our project to MVC format. Still having some issues with CareKit store though.

--- Health Journal/AppDelegate.swift
+++ Health Journal/AppDelegate.swift
@@ -7,22 +7,32 @@
 //
 
 import UIKit
+import CareKit
+import Contacts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    // Manages synchronization of a CoreData store
+    lazy var synchronizedStoreManager: OCKSynchronizedStoreManager = {
+        
+        //OCKStore is basically just a on-device database, here we are creating it
+        //We can use any other data base as well as long as it works with OCKStore protocol
+        let store = OCKStore(name: "SampleAppStore")
+        
+        store.populateSampleData()
+        
+        //OCKSynchronizedStoreManager is what makes it possible to use another database
+        let manager = OCKSynchronizedStoreManager(wrapping: store)
+        return manager
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -32,6 +42,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
+
+private extension OCKStore {
+
+    // Add tasks and contacts into the store in this function
+    func populateSampleData() {
+
+        let currentDate = Calendar.current.startOfDay(for: Date())
+        let afternoonAtSevenMedication = Calendar.current.date(byAdding: .hour, value: 19, to: currentDate)!
+        
+        let schedule = OCKSchedule(composing: [OCKScheduleElement(start: afternoonAtSevenMedication, end: nil, interval: DateComponents(day: 1))])
+        
+        var medication = OCKTask(id: "levetiracetam", title: "Take Levetiracetam Medication", carePlanID: nil, schedule: schedule)
+        
+        medication.instructions = "Take 2500mg of levetiracetam everyday at 7pm"
+        
+    
+        
+        addTask(medication)
+        
+    }
+}
+
 
---
+++ Health Journal/Controller/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  Health Journal
+//
+//  Created by Sadrac Tijerina on 8/26/20.
+//  Copyright © 2020 M-A-I-D-S. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CareKit
+
+//OCKDailyViewController is a view controller that displays a calender in the header, and a page view controller in the body
+class ContentView: OCKDailyPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //This is creating the care team button from the top right and calls the presentContactdListViewController when clicked
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(title: "Care Team", style: .plain, target: self,
+                            action: #selector(presentContactsListViewController))
+    }
+
+    //This is how we are displaying contacts
+    @objc private func presentContactsListViewController() {
+        //This is creating the viewController which displays the contacts, we just need to pass the storeManager
+        //Store manager gets intialized and created on an OCKDailyPageViewController
+        let viewController = OCKContactsListViewController(storeManager: storeManager)
+        
+        //This is once they are in the contact page. The title will be Care team and to exit there will be a done button to the right
+        viewController.title = "Health Team"
+        
+        // modalInPresentation is set on the view controller when you wish to force the presentation hosting the view controller into modal behavior. When this is active, the presentation will prevent interactive dismiss and ignore events outside of the presented view controller's bounds until this is set to NO.
+        
+        viewController.isModalInPresentation = true //still dont know what this does comment above I found in their definition
+        viewController.navigationItem.rightBarButtonItem =
+            UIBarButtonItem(title: "Done", style: .plain, target: self,
+                            action: #selector(dismissContactsListViewController))
+
+         
+        let navigationController = UINavigationController(rootViewController: viewController) // this is what makes the view look stacked on top of one another
+        present(navigationController, animated: true, completion: nil) //Now it shows the view, completion is what we plan to execute after the presentation finishes
+    }
+
+    @objc private func dismissContactsListViewController() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    // This will be called each time the selected date changes.
+    // Use this as an opportunity to rebuild the content shown to the user.
+    //When the CareViewController class is called/used this function gets used, listViewController parameter is what we populate with content, dailyPageViewController is what the app provides as content, and date is a date that determines the content to insert into the list view cnotroller
+    
+    override func dailyPageViewController(_ dailyPageViewController: OCKDailyPageViewController,
+                                          prepare listViewController: OCKListViewController, for date: Date) {
+
+        let identifiers = ["levetiracetam"]
+        var query = OCKTaskQuery(for: date) //A query that limits which task your fetch returns
+        query.ids = identifiers //This looks for tasks that have the id listed in the variable identifiers. The tasks were created in AppDelegate.swift
+        query.excludesTasksWithNoEvents = true //False is default
+
+        storeManager.store.fetchAnyTasks(query: query, callbackQueue: .main) { result in //fetchAnyTasks retrieves a set of stack from the store
+            switch result {
+            case .failure(let error): print("Error: \(error)")
+            case .success(let tasks):
+
+
+                // Create a card for the levetiracetam task if there are events for it on this day.
+                if let levetiracetamTask = tasks.first(where: { $0.id == "levetiracetam" }) {
+                    let levetiracetamCard = OCKChecklistTaskViewController(task: levetiracetamTask, eventQuery: .init(for: date),
+                                                                        storeManager: self.storeManager)
+                    listViewController.appendViewController(levetiracetamCard, animated: false)
+                }
+
+            }
+        }
+    }
+}
